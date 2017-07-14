18b6910258bb91a5f522de40e349ebd77f22be72
Hao Jiang <jiang@sansan.com>
2017-07-14 23:03:03 +0900

    Fix bug

--- git_checkout.sh
+++ git_checkout.sh
@@ -4,5 +4,6 @@ paste <(git branch | wc -l | xargs seq) <(git branch)
 echo -n "Input the branch number to checkout: "
 read branch_num
 
-git branch | sed -e "s/[\* ]*//g" | sed "$branch_num"'!d' | git checkout
+branch_name=`git branch | sed -e "s/[\* ]*//g" | sed "$branch_num"'!d'`
+git checkout $branch_name
 
